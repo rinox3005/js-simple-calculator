705853a70f377ad80396ac2a33c6a183c5fa16d6
rinox3005 <peppe.rinox@gmail.com>
2024-05-09 17:30:20 +0200

    [UPD] milestone 2 done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 'use strict';
 
-const result = document.getElementById('result');
+const screen = document.getElementById('result');
+let numberOnScreen = '';
 let firstNumber = '';
 let secondNumber = '';
-const selectedOperator = '';
+let selectedOperator = '';
 const digits = document.querySelectorAll('.digit');
 const numDigits = digits.length;
+const operator = document.querySelectorAll('.operator');
+const numOperator = operator.length;
 
 // seleziono tutti i bottoni che contengono numeri e faccio fare qualcosa al click
-for (let i = 0; i < numDigits; i++) {
-    digits[i].addEventListener('click', function () {
-        firstNumber += digits[i].innerHTML;
-        result.innerHTML = firstNumber;
+function keypad() {
+    for (let i = 0; i < numDigits; i++) {
+        digits[i].addEventListener('click', function () {
+            numberOnScreen += digits[i].innerHTML;
+            screen.innerHTML = numberOnScreen;
+        })
+    }
+}
+
+// seleziono tutti gli operatori e salvo il valore di quello cliccato in una variabile selectedOperator
+for (let i = 0; i < numOperator; i++) {
+    operator[i].addEventListener('click', function () {
+        keypad();
+        selectedOperator = operator[i].value;
+        firstNumber = numberOnScreen;
+        console.log(selectedOperator);
+        console.log(firstNumber);
+
     })
 }
+
+keypad();
