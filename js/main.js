5bf9108706f3b484fa6d546390f5eb0348c1b9bb
rinox3005 <peppe.rinox@gmail.com>
2024-05-09 16:41:05 +0200

    [UPD] milestone 1 done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const result = document.getElementById('result');
+let firstNumber = '';
+let secondNumber = '';
+const selectedOperator = '';
+const digits = document.querySelectorAll('.digit');
+const numDigits = digits.length;
+
+// seleziono tutti i bottoni che contengono numeri e faccio fare qualcosa al click
+for (let i = 0; i < numDigits; i++) {
+    digits[i].addEventListener('click', function () {
+        firstNumber += digits[i].innerHTML;
+        result.innerHTML = firstNumber;
+    })
+}
